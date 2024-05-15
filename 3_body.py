f1cd8bed9c9edb3561212d8a7d33a9b6b86bcb50
Simone <ciciriellosimone@gmail.com>
2024-05-16 00:28:13 +0200

    Comments removed

--- 3_body.py
+++ 3_body.py
@@ -122,45 +122,4 @@ for ax in fig.get_axes():
     ax.label_outer()
 plt.show()
 
-'''
-fig, (ax1, ax2) = plt.subplots(2)
-fig.suptitle('Vertically stacked subplots')
-ax1.plot(x, y)
-ax2.plot(x, -y)
 
-
-
-
-
-
-fig, axs = plt.subplots(2, 2)
-axs[0, 0].plot(x, y)
-axs[0, 0].set_title('Axis [0, 0]')
-axs[0, 1].plot(x, y, 'tab:orange')
-axs[0, 1].set_title('Axis [0, 1]')
-axs[1, 0].plot(x, -y, 'tab:green')
-axs[1, 0].set_title('Axis [1, 0]')
-axs[1, 1].plot(x, -y, 'tab:red')
-axs[1, 1].set_title('Axis [1, 1]')
-
-for ax in axs.flat:
-    ax.set(xlabel='x-label', ylabel='y-label')
-
-# Hide x labels and tick labels for top plots and y ticks for right plots.
-for ax in axs.flat:
-    ax.label_outer()
-    
-'''
-
-# plot_data(time_frames, mec_en_data, mec_label, 'black')
-# plt.draw()
-
-# fig, ((ax1, ax2), (ax3, ax4)) = plt.subplots(2, 2)
-# fig.suptitle('Sharing x per column, y per row')
-# ax1.plot(x, y)
-# ax2.plot(x, y**2, 'tab:orange')
-# ax3.plot(x, -y, 'tab:green')
-# ax4.plot(x, -y**2, 'tab:red')
-#
-# for ax in fig.get_axes():
-#     ax.label_outer()
